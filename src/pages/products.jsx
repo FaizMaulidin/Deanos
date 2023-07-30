6d1402f9f0a478d8ebc87e41c192f1be83ded745
Faiz MD <famuld14@gmail.com>
2023-07-30 11:02:27 +0700

    Move DOM selection to useEffect

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { UsingDucts, addCart, addWish, ray, removeWish } from '../components/api'
 import { Navbar } from '../components/navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,10 +6,19 @@ import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons'
 import Footer from '../components/footer'
 
 const Products = (props) => {
+   const [getInp, setInp] = useState()
+   const [love, setLove] = useState()
+
+   useEffect(() => {
+      const inp = document.querySelector('.addcart input')
+      const loveEl = document.querySelector('.love')
+      setInp(inp)
+      setLove(loveEl)
+   })
+
    return UsingDucts().map((prod) => {
       if (prod.id == props.id) {
-         const inp = document.querySelector('.addcart input')
-         const love = document.querySelector('.love')
+         
          ray.map((item) => {
             if (prod.id === item.productId && item.wishlist === true) {
                if (love) {
@@ -58,29 +67,29 @@ const Products = (props) => {
                      <div className="addcart flex flex-col items-end mt-3 gap-2">
                         <div className="count flex justify-between text-4xl items-center border-[1px] px-5 py-0 rounded-[0.25rem] h-10 border-zinc-400 text-rose-700 w-6/12 sm:w-4/12">
                            <button onClick={() => {
-                              let count = parseInt(inp.value)
+                              let count = parseInt(getInp.value)
                               if (count > 1) {
                                  count -= 1
                               } else { count = '' }
-                              inp.value = count
+                              getInp.value = count
                            }} className="leading-5 mb-1">-</button>
                            <input type="number" className="qty w-[5ch] text-zinc-700 bg-zinc-100 text-2xl text-center rounded-md h-5" placeholder='0' />
                            <button onClick={() => {
                               let count = 0
-                              if (inp.value === '') {
+                              if (getInp.value === '') {
                                  count = 0
                               } else {
-                                 count = parseInt(inp.value)
+                                 count = parseInt(getInp.value)
                               }
                               count += 1
-                              inp.value = count
+                              getInp.value = count
                            }} className="mb-1.5 leading-5">+</button>
                         </div>
                         <button id='btn-add-cart' onClick={() => {
                            let count = 0
-                           if (inp.value == 0) {
+                           if (getInp.value == 0) {
                               count = 0
-                           } else {count = parseInt(inp.value)}
+                           } else {count = parseInt(getInp.value)}
                            addCart(prod.id, count)
                            added()
                         }} className='rounded-[0.25rem] font-semibold text-rose-700 border-rose-700 border-2 text-lg w-1/2 h-10 sm:w-4/12 transition-all duration-500 ease-in-out'>Add to cart</button>
